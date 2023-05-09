551883ed5ee73d4061a739624e0e99382a4ee2c3
Artem <a.ku666@vk.com>
2023-05-09 20:59:18 +0200

    Change

---
+++ app/app.js
@@ -0,0 +1,4 @@
+import {Game, GameSavingData, loadGame, saveGame } from "../game/game.js";
+
+const game = new Game();
+game.start();
---
+++ game/game.js
@@ -0,0 +1,17 @@
+import Character from '../domain/domain.js';
+
+export {Game, GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame}
+ class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
